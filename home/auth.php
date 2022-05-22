19696b2b13008741e4ccf85e5d55cb0c5342f908
Sanket Khaire <69530002+Sanketkhaire@users.noreply.github.com>
2022-05-23 00:24:59 +0530

    Update auth.php

--- home/auth.php
+++ home/auth.php
@@ -1,6 +1,8 @@
 <?php
 	//Start session
-	session_start();
+	if (session_status() == PHP_SESSION_NONE) {
+      session_start();
+   }
 	
 	//Check whether the session variable SESS_MEMBER_ID is present or not
 	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
