a63cc28c5b05972e9ff1c802b59c996e88950e6b
Sanket Khaire <69530002+Sanketkhaire@users.noreply.github.com>
2022-05-23 00:23:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,12 +12,14 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Public Distribution System</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Koulen&family=Lato:wght@100;300;400;700;900&family=Noto+Sans:wght@500;600&family=Raleway:ital,wght@0,900;1,700&family=Roboto:wght@500&family=Source+Sans+Pro:wght@200;400;600;700;900&display=swap" rel="stylesheet">
 <link href="styles/layout1.css" rel="stylesheet" type="text/css" />
 <link href="styles/forms.css" rel="stylesheet" type="text/css" />
 </head>
 <body >
 <div id="header"> 
-<p></p>
 <h1><strong>Public Distribution System</strong></h1>
 
 </div>
@@ -30,16 +32,15 @@
 
                   <fieldset>
 				  <table align="center">
-                <legend style="background-color:purple; width:800px; color:white; border-radius: 10px;">Please login here!</legend>
-				
+                <legend style="background-color:#2F8F9D; width:800px;height:20px; color:white; border-radius: 10px;padding-left:10px;padding-top:5px;font-family:'Source Sans Pro', sans-serif;">Please login here!</legend>
 				
 				<tr>
-          <td><label>Username:<label></td>
-          <td ><input type="text" autocomplete="off" name="user" tabindex="2" required /></td>
+          <td><label>Username<label></td>
+          <td ><input type="text" autocomplete="off" name="user" tabindex="2" autocomplete="off" required/></td>
         </tr>
 			 <tr>
-          <td ><label>Password:</label></td>
-          <td width="195"><input type="password" name="pass" tabindex="2" required />
+          <td ><label>Password</label></td>
+          <td width="195"><input type="password" name="pass" tabindex="2" autocomplete="off" required/>
               <br />
 
               <font color="red" size="1">
@@ -53,7 +54,7 @@
         </tr>
         <tr>
           <td>&nbsp;</td>
-          <td><input type="submit" style="background-color:green; width: 260px; color:white; border-radius: 10px;" name="Submit" value="Login" tabindex="2" id="Submit" /></td>
+          <td><input type="submit" style="background-color:#242F9B; height:30px;width: 260px; color:white; border-radius: 10px;" name="Submit" value="Login" tabindex="2" id="Submit" /></td>
         </tr>
 				
 				</table>
@@ -64,10 +65,6 @@
 </div>
 <div id="footer"> 
 <p>Copy right! <?php echo date('d-m-Y');?></p>
-
-
-
-
 </div>
 </body>
-</header>
+</html>
